8b1eac0ddd771e1feb17fdcf5210be182f2e6cbe
ThisIsDiKo <38630172+ThisIsDiKo@users.noreply.github.com>
2021-03-15 17:54:28 +0300

    М

--- parse_cvs_files.py
+++ parse_cvs_files.py
@@ -1,15 +1,33 @@
 import csv
 
+folder_name = 'csv_orig/'
 filenames = [
+    '2018 140-2.csv',
+    '2019 140-2.csv',
+    '2020 140-2.csv',
+
+    '2018 140-3.csv',
+    '2019 140-3.csv',
+    '2020 140-3.csv',
+
+    '2018 160D1.csv',
+    '2019 160D1.csv',
+    '2020 160D1.csv',
+
     '2018 180D1.csv',
     '2019 180D1.csv',
-    '2020 180D1.csv'
+    '2020 180D1.csv',
+
+    '2018 250-2.csv',
+    '2019 250-2.csv',
+    '2020 250-2.csv'
 ]
 
 for filename in filenames:
+    fullFileName = folder_name + filename
     print(filename)
     data = []
-    with open(filename) as csvfile:
+    with open(fullFileName) as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';')
         t = []
         for row in spamreader:
@@ -17,9 +35,16 @@ for filename in filenames:
             t.append(row)
 
         for i in range(4, len(t[0]), 3):
-            print('week: {0} {1} {2}'.format(int(i/3), t[3][i-1], t[3][i]))
+            #print('week: {0} {1} {2}'.format(int(i/3), t[3][i-1], t[3][i]))
             slice = []
-            slice.append(int(i/3))
+
+            if t[0][i-2].isdigit():
+                v = int(t[0][i-2]) - 1
+                v = str(v)
+                slice.append(v)
+            else:
+                slice.append(t[0][i - 2])
+
             if t[3][i - 2] == '':
                 slice.append('0')
             else:
@@ -43,7 +68,7 @@ for filename in filenames:
         print(slice)
 
 
-    f_name = '{0}.csv'.format(filename.split('.')[0]+'_formatted')
+    f_name = 'csv_formatted/{0}.csv'.format(filename.split('.')[0]+'_formatted')
 
     with open(f_name, 'w') as file:
         writer = csv.writer(file, lineterminator="\r")
